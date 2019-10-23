cd547fc5a0c5333ff2f1aa4530d86484be11cf5c
quaide <tranterq@gmail.com>
2019-10-23 16:21:19 -0400

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+
+public class Controller {
+    @FXML
+    private TextField productName;
+    @FXML
+    private TextField productManufacturer;
+    @FXML
+    private ChoiceBox itemType;
+
+    private DatabaseManager databaseManager = new DatabaseManager();
+
+    public Controller() throws SQLException {
+    }
+
+    public void addProduct() {
+        String name = productName.getText();
+        String manufacturer = productManufacturer.getText();
+        String type = itemType.toString();
+
+        databaseManager.insert(type, manufacturer, name);
+        System.out.println("Button Pressed");
+    }
+
+    public void recordProduction(ActionEvent actionEvent) {
+        System.out.println("Button Pressed");
+    }
+}
---
+++ src/sample/DatabaseManager.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:D:/trant/Documents/Java Practice/OraclProduction/res/userDatabase";
+    private Connection con;
+
+    public DatabaseManager() throws SQLException {
+        con = DriverManager.getConnection(DB_URL, "", "");
+    }
+
+    public void insert(String type, String manufacturer, String name) {
+        try {
+            //String sql = "INSERT INTO " + table + " VALUES (?, ?, ?)";
+            String sql = "INSERT INTO PRODUCT(type, manufacturer, name) VALUES (?, ?, ?)";
+
+            PreparedStatement insertUser = con.prepareStatement(sql);
+
+            insertUser.setString(1, type);
+            insertUser.setString(2, manufacturer);
+            insertUser.setString(3, name);
+
+            insertUser.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
