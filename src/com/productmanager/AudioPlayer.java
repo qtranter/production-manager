71148248ba13ef48db0a028fc36685be02b4d4ab
quaide <tranterq@gmail.com>
2019-10-24 23:33:49 -0400

    Completed Issues 1, 2, annd 3

---
+++ src/com/productmanager/AudioPlayer.java
@@ -0,0 +1,38 @@
+package com.productmanager;
+
+public class AudioPlayer extends Product implements MultimediaControl {
+    String audioSpecification;
+    String mediaType;
+
+    public AudioPlayer(String name, String manufacturer, String audioSpecification) {
+        super(name, manufacturer, ItemType.AUDIO.toString());
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing!");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopped!");
+    }
+
+    @Override
+    public void previous() {
+        System.out.println("Previous!");
+    }
+
+    @Override
+    public void next() {
+        System.out.println("Next!");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "audioSpecification='" + audioSpecification + '\'' +
+                ", mediaType='" + mediaType + '\'' +
+                '}';
+    }
+}
